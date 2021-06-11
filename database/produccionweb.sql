04bfa44048b76ca7fe802d12ce7f2a5f5c2f3703
BrianRomano <brianromanook@gmail.com>
2021-06-11 19:38:13 -0300

    Cambio en base de datos

--- database/produccionweb.sql
+++ database/produccionweb.sql
@@ -1,19 +1,12 @@
 CREATE DATABASE produccionweb;
 USE produccionweb;
 
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 -- TABLA CATEGORIAS
 CREATE TABLE `categories` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `activo` tinyint
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `activo` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `categories` (`id`, `nombre`, `activo`) VALUES
 (1, 'Apple', 1),
@@ -21,60 +14,60 @@ INSERT INTO `categories` (`id`, `nombre`, `activo`) VALUES
 (3, 'Xiaomi', 1),
 (4, 'Huawei', 1);
 
-
--- TABLA MODELOS 
-CREATE TABLE `models` (
-  `id` int NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `activo` tinyint
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `models` (`id`, `nombre`, `activo`) VALUES
-(1, 'iPhone 11 Pro', 1),
-(2, 'iPhone X', 1),
-(3, 'iPhone 8', 1),
-(4, 'iPhone 8 Plus', 1),
-(5, 'S10e', 1),
-(6, 'S10 Plus', 1),
-(7, 'Note 10', 1),
-(8, 'A50', 1),
-(9, 'Mi 9', 1),
-(10, 'Mi 9T', 1),
-(11, 'P30', 1),
-(12, 'P30 II', 1);
-
-
 -- TABLA COMENTARIOS
 CREATE TABLE `comments` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `comentario` text NOT NULL,
-  `rank` int NOT NULL,
-  `fecha` datetime,
-  `producto` int NOT NULL,
+  `rank` int(11) NOT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `producto` int(11) NOT NULL,
   `ip` varchar(15) NOT NULL,
-  `activo` tinyint
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `activo` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- TABLA MODELOS
+CREATE TABLE `models` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `activo` tinyint(4) DEFAULT NULL, 
+  `categoria` int(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `models` (`id`, `nombre`, `activo`, `categoria`) VALUES
+(1, 'iPhone 11 Pro', 1, 1),
+(2, 'iPhone X', 1, 1),
+(3, 'iPhone 8', 1, 1),
+(4, 'iPhone 8 Plus', 1, 1),
+(5, 'S10e', 1, 2),
+(6, 'S10 Plus', 1, 2),
+(7, 'Note 10', 1, 2),
+(8, 'A50', 1, 2),
+(9, 'Mi 9', 1, 3),
+(10, 'Mi 9T', 1, 3),
+(11, 'P30', 1, 4),
+(12, 'P30 II', 1, 4);
 
 
 -- TABLA PERFILES
 CREATE TABLE `perfiles` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `perfiles` (`id`, `nombre`) VALUES
 (1, 'Administrador'),
 (2, 'Filtros'),
-(3, 'Ventas'), 
+(3, 'Ventas'),
 (4, 'Reseñas');
 
 
 -- TABLA PERFIL_PERMISOS
 CREATE TABLE `perfil_permisos` (
-  `perfil` int NOT NULL,
-  `permiso` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `perfil` int(11) NOT NULL,
+  `permiso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `perfil_permisos` (`perfil`, `permiso`) VALUES
 (1, 1),
@@ -85,11 +78,11 @@ INSERT INTO `perfil_permisos` (`perfil`, `permiso`) VALUES
 
 -- TABLA PERMISOS
 CREATE TABLE `permisos` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `code` varchar(10) NOT NULL,
   `module` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `permisos` (`id`, `nombre`, `code`, `module`) VALUES
 (1, 'Administrador', 'a.a', 'adm'),
@@ -100,16 +93,16 @@ INSERT INTO `permisos` (`id`, `nombre`, `code`, `module`) VALUES
 
 -- TABLA PRODUCTOS
 CREATE TABLE `productos` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `descripcion` text NOT NULL,
-  `precio` float(100, 0) NOT NULL, 
-  `imagen` varchar(255), 
-  `activo` tinyint,
-  `destacado` tinyint,
-  `categoria` int NOT NULL, 
-  `modelo` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `precio` float(100,0) NOT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
+  `activo` tinyint(4) DEFAULT NULL,
+  `destacado` tinyint(4) DEFAULT NULL,
+  `categoria` int(11) NOT NULL,
+  `modelo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `productos` (`id`, `titulo`, `descripcion`, `precio`, `imagen`, `activo`, `destacado`, `categoria`, `modelo`) VALUES
 (1, 'iPhone 11 Pro', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita commodi minus nemo? Non aspernatur sunt autem quisquam sed, consequatur corporis tenetur beatae eius similique rerum sequi aliquam at laborum. Deleniti!', 1100, 'iphone-11-pro.jpg', 1, 1, 1, 1),
@@ -128,31 +121,44 @@ INSERT INTO `productos` (`id`, `titulo`, `descripcion`, `precio`, `imagen`, `act
 
 -- TABLA USUARIOS
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `email` varchar(200) NOT NULL,
   `user` varchar(30) NOT NULL,
   `password` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `users` (`id`, `nombre`, `email`, `user`, `password`) VALUES
+(27, 'Admin', 'admin@admin.com', 'admin', '$2y$10$YN0mb.Rc7pyaUhketa.KJugs3PazMe6BB5AC5gl.MD2mG5hnNPLti'),
+(28, 'Brian', 'brian@brian.com', 'brian', '$2y$10$yzUITjpgANM9j/FkIDoyhekWkUk9jl62hvsMTNt0BQxmUx/aKtWVy'),
+(29, 'Leonel', 'leonel@leonel.com', 'leonel', '$2y$10$5NDw/nGwlml56W.bZZoOO.7xGUCsDXrNZpb9sdq7lqQ8yGKmON6Ye'),
+(30, 'Romano', 'romano@romano.com', 'romano', '$2y$10$Rt0WT/NVL6I4wMpjgyAAde8bH6cm4rv8j/sdlIdDlGmXcuznhRUw.');
 
 
--- TABLA USUARIO_PERFIL
+-- TABLA USUARIOS_PERFIL
 CREATE TABLE `user_perfil` (
-  `user` int NOT NULL,
-  `perfil` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `user` int(11) NOT NULL,
+  `perfil` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `user_perfil` (`user`, `perfil`) VALUES
+(27, 1),
+(28, 2),
+(29, 3),
+(30, 4);
 
 
--- LLAVES PRIMARIAS 
+-- LLAVES PRIMARIAS
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `models`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `producto` (`producto`);
+
+ALTER TABLE `models`
+  ADD PRIMARY KEY (`id`)
+  ADD KEY `categoria` (`categoria`);;
 
 ALTER TABLE `perfiles`
   ADD PRIMARY KEY (`id`);
@@ -175,35 +181,37 @@ ALTER TABLE `users`
 ALTER TABLE `user_perfil`
   ADD PRIMARY KEY (`user`,`perfil`);
 
--- ID's AUTOINCREMENTABLES
-ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
-ALTER TABLE `models`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+-- AUTOINCREMENTAR 'ID'
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 ALTER TABLE `comments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `models`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 ALTER TABLE `perfiles`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `permisos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 ALTER TABLE `productos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 
 -- LLAVES FORANEAS
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`producto`) REFERENCES `productos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`producto`) REFERENCES `productos` (`id`);
+
+ALTER TABLE `models`
+  ADD CONSTRAINT `models_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categories` (`id`);
 
 ALTER TABLE `productos`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-   ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`modelo`) REFERENCES `models` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-COMMIT;
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`modelo`) REFERENCES `models` (`id`);
